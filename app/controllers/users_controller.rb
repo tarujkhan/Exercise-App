bd240144a0bab6f94650100013a81b0283bf296b
taruj khan <tarujkhan@DESKTOP-E8JDKB5.localdomain>
2020-06-04 12:44:49 -0500

    Finished my users controller. Now on to the next.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,3 +10,39 @@ end
 
     post '/register' do
 @user = User.new(params)
+@errors = @user.errors.full_messages 
+erb: "users/create_user"
+else
+    session[:user_id] = @user.id 
+    redirect to 'work_outs'
+end
+end
+
+get '/login' do 
+    if !session[:user_id] 
+        erb :'users/login'
+    else 
+        redirect to '/work_outs'
+    end
+end
+
+post '/login'
+@user = User.find_by(username: params[username])
+if @user && @user.authenticate(params[password])
+    session[:user_id] = @user_id
+    redirect to '/work_outs'
+else
+    @errors = "The username or password is not correct"
+    erb :/'login'
+end
+end
+
+get '/logout'
+if logged_in?
+    session[:user_id] = nil
+    session.destroy
+    redirect '/login'
+end
+end
+end 
+
