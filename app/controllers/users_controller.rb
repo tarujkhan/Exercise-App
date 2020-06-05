c22b09f9afee775993328300cf4f55ccbdef2cbd
taruj khan <tarujkhan@DESKTOP-E8JDKB5.localdomain>
2020-06-05 09:58:51 -0500

    Still working on errors. Just wrote all the methods.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,15 @@
     class UsersController < ApplicationController
 
-       get 'users/:slug' do
-        @user = User.find_by_slug(params[:slug])
-        erb :'views/showwork_out'
-       end
+    #    get 'users/:slug' do
+    #     @user = User.find_by_slug(params[:slug])
+    #     erb :'views/showwork_out'
+    #    end
     
 
        get '/register' do 
-        if !logged_in?
-            #if !session[:user_id]
+        
+        #if !logged_in?
+            if !session[:user_id]
             erb :'user/create_user'
         else 
             redirect to '/work_outs' 
@@ -16,22 +17,23 @@
         end
 
         post '/register' do 
-            if params[:user_name] == "" || params[:email] == "" || params[:password]
-            #@user = User.new(params)
-            #if !@user.save
-            #@errors = @user.errors.full_messages 
+            #if params[:user_name] == "" || params[:email] == "" || params[:password]
+            @user = User.new(params)
+            if !@user.save
+            @errors = @user.errors.full_messages 
             erb :"user/create_user"
+            #redirect to '/register'
          else
-            @user = User.new(:user_name => params[:user_name], :email => params[:email], :password => params[:password])
-            @user.save
+            #@user = User.new(:user_name => params[:user_name], :email => params[:email], :password => params[:password])
+            #@user.save
             session[:user_id] = @user.id 
             redirect to '/work_outs'
             end
         end
 
         get '/login' do
-            if !logged_in? 
-            #if !session[:user_id] 
+            #if !logged_in? 
+            if !session[:user_id] 
             erb :'user/login'
         else 
             redirect to '/work_outs'
@@ -39,7 +41,7 @@
         end
 
         post '/login' do
-        @user = User.find_by(:user_name => params[:user_name])
+        @user = User.find_by(params[:user_name])
             if @user && @user.authenticate(params[:password])
             session[:user_id] = @user_id
             redirect to '/work_outs'
@@ -51,13 +53,13 @@
 
         get '/logout' do
             if logged_in?
-            #@user = current_user
-            #@user = nil
+            @user = current_user
+            @user = nil
             session.destroy
-            redirect to '/login'
+            redirect to '/'
         else 
             redirect to '/'
             end
         end
-    end
-
+    
+    end 
--- app/controllers/work_outs_controller.rb
+++ app/controllers/work_outs_controller.rb
@@ -14,7 +14,7 @@
              get '/work_outs/new' do 
                 if logged_in?
                 @current_user
-                @workout = Workout.new(params[:workout])
+                @workout = WorkOut.new(params[:workout])
            #workout.user_id = current_user.id
                 erb :'/workou_ts/newwork_out'
             else
@@ -37,11 +37,51 @@
             end
 
             get '/work_outs/:id' do 
-                @workout = Workout.find(params[:id])
+                @workout = WorkOut.find(params[:id])
                     if logged_in? && @workout.user == current_user
                     erb :'/workou_ts/showwork_out' 
                 else
                     redirect to '/login'
                     end
+                end
+
+            get '/work_outs/:id/edit' do
+                   @workout = WorkOut.find(params[:id])
+                    if logged_in? && @workout.user == current_user
+                      workout = WorkOut.find(params[:id])
+                      @user = User.find(session[:user_id])
+                      erb :'/workou_ts/editwork_out'
+                  else
+                    redirect to '/login'
+                end
+            end
+                
+                
+            patch '/work_outs/:id' do
+                    @workout = WorkOut.find(params[:id])
+                    @workout.name = params[:name]
+                    @workout.date = params[:date]
+                    @expense.numberofreps = params[:numberofreps]
+                    @expense.duration = params[:duration]
+                
+                    if !@expense.save
+                    @errors = @workout.errors.full_messages
+                    erb :'/workou_ts/editwork_out'
+                    else
+                    redirect to("/work_outs/#{@workout.id}")
+                
+                    end
+                
+                  end
+                               
+                
+                  delete '/work_outs/:id/delete' do
+                    @workout = WorkOut.find(params[:id])
+                    if logged_in? && @workout.user == current_user
+                    @workout.destroy
+                    redirect to 'work_outs'
+                   else
+                   redirect to '/login'
+                   end
                 end
             end 
