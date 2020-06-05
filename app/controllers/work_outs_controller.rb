d6ad8ac6fec7ba8e6a68fe357003781c5d49d861
taruj khan <tarujkhan@DESKTOP-E8JDKB5.localdomain>
2020-06-04 22:41:57 -0500

    One view is working but the other isn't. Still working on it.

--- app/controllers/work_outs_controller.rb
+++ app/controllers/work_outs_controller.rb
@@ -1,47 +1,47 @@
-class WorkOutController < ApplicationController
+        class WorkOutController < ApplicationController
 
-    get '/work_outs' do 
-        if logged_in? 
-            @workout = current_user.workout
+            get '/work_outs' do 
+                if logged_in? 
+                @workout = current_user.workout
            #@workout = WorkOut.where(current_user.id: workout.user_id)
-            erb :'/workou_ts/showwork_out'
-        else 
-            redirect to '/login'
-        end
-end
+                 erb :'/workou_ts/showwork_out'
+            else 
+                redirect to '/login'
+                end
+            end
 
 
-    get '/work_outs/new' do 
-        if logged_in?
-            @current_user
-            @workout = Workout.new(params[:workout])
+             get '/work_outs/new' do 
+                if logged_in?
+                @current_user
+                @workout = Workout.new(params[:workout])
            #workout.user_id = current_user.id
-    erb :'/workou_ts/newwork_out'
-        else
-            redirect to '/login'
-        end
-    end
+                erb :'/workou_ts/newwork_out'
+            else
+                redirect to '/login'
+                end
+            end
 
-    post '/work_outs' do 
-        if logged_in?
-            @workout = current_user.work_outs.build(params)
-            if !@workout.save
+             post '/work_outs' do 
+                if logged_in?
+                @workout = current_user.work_outs.build(params)
+                if !@workout.save
                 @errors = @workout.errors.full_messages
                 erb :'/workou_ts/newwork_out'
             else 
                 redirect to '/work_outs'
+                end
+                 else
+                    redirect to '/login'
+                end
             end
-        else
-            redirect to '/login'
-        end
-    end
 
-    get '/work_outs/:id' do 
-        @workout = Workout.find(params[:id])
-        if logged_in? && @workout.user == current_user
-            erb :'/workou_ts/showwork_out' 
-        else
-            redirect to '/login'
-        end
-    end
-end 
+            get '/work_outs/:id' do 
+                @workout = Workout.find(params[:id])
+                    if logged_in? && @workout.user == current_user
+                    erb :'/workou_ts/showwork_out' 
+                else
+                    redirect to '/login'
+                    end
+                end
+            end 
