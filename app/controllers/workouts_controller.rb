032d948a2ac074244cde5ef9a61073106c9c694a
taruj khan <tarujkhan@DESKTOP-E8JDKB5.localdomain>
2020-06-23 14:51:30 -0500

    Making more changes and looking much better. The layout is better looking now.

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,25 +1,9 @@
         class WorkoutsController < ApplicationController
-
-            # http verbs 'the route' do 
-            #     if logged_in? - to check if user is logged
-            #         logic depending on the verb goes here
-            #         the view erb related to that logic goes here 
-            #     else 
-            #         redirect to login page 
-           #============================
-            #show all user workouts on the index page including current user work outs
-                #this particular thing is related to the index route showing all workouts
-
-            #on the show page of each individual workouts only show edit and delete button for current user workouts
-            #the show page for each work out that isn't created by current user does not have edit or delete actions
-            #what condition can I write on the erb to only edit my workouts and not someone else's?
-            #if this particular workout is not connected to the current user do not show edit or delete? otherwise show me.
-
+           
             get '/work_outs' do 
                 if logged_in? 
                @workout = current_user.workouts && Workout.all
-               #@workouts = Workout.all
-                erb :'/work_outs/index'
+               erb :'/work_outs/index'
             else 
                 redirect_if_not_logged_in
                 end
@@ -37,13 +21,13 @@
 
              post '/work_outs' do 
                 if logged_in?
-                @workout = current_user.workouts.build(params)
-                if !@workout.save
-                @errors = @workout.errors.full_messages
-                erb :'/work_outs/newwork_out'
+                     @workout = current_user.workouts.build(params)
+                if  !@workout.save
+                     @errors = @workout.errors.full_messages
+                     erb :'/work_outs/newwork_out'
             else 
-                redirect to '/work_outs'
-                end
+                    redirect to '/work_outs'
+                    end
                  else
                     redirect_if_not_logged_in
                 end
@@ -60,9 +44,8 @@
             end
 
             get '/work_outs/:id' do 
-                @workout = Workout.find(params[:id])
-                #binding.pry
-                   if logged_in? 
+                 @workout = Workout.find(params[:id])
+                if logged_in? 
                     erb :'/work_outs/showwork_out' 
                 else
                     redirect to '/login'
@@ -74,25 +57,15 @@
             get '/work_outs/:id/edit' do
                 @workout = Workout.find(params[:id])
                     if logged_in? && @workout.user == current_user
-                      #@workout = Workout.find(params[:id])
-                     # @user = User.find(session[:user_id])
-                      erb :'/work_outs/editwork_out'
+                       erb :'/work_outs/editwork_out'
                   else
-                    redirect_if_not_logged_in
-                end
+                       redirect_if_not_logged_in
+                     end
             end
                 
                 
             patch '/work_outs/:id' do
-                # if current_user.id == workouts.user_id
-                #     @workout = Workout.find(params[:id])
-                #     @errors
-
-                #    erb: '/work_outs/editwork_out.erb' 
-                # else 
-                    
-                #     redirect to :'/index'
-                @workout = Workout.find(params[:id])
+                    @workout = Workout.find(params[:id])
                 if logged_in? && @workout.user == current_user
                  
                     @workout = Workout.find(params[:id])
@@ -118,21 +91,23 @@
                    else
                     redirect_if_not_logged_in
                    end
-                end
+                  end
 
                 get '/advice' do
                     erb :'/work_outs/advice'
-            end 
+                 end 
 
-    #             get '/homepage' do
-                    
-    #                 if logged_in?
-    #                     @user = User.find_by(user_name: params[:user_name])
-    #                 @workouts = Workout.all
-    #                 erb :'/work_outs/homepage'
-                    
-    #                 else 
-    #                     redirect_if_not_logged_in
-    #     end
-    # end 
-end
+    # http verbs 'the route' do 
+            #     if logged_in? - to check if user is logged
+            #         logic depending on the verb goes here
+            #         the view erb related to that logic goes here 
+            #     else 
+            #         redirect to login page 
+           #============================
+            #show all user workouts on the index page including current user work outs
+                #this particular thing is related to the index route showing all workouts
+
+            #on the show page of each individual workouts only show edit and delete button for current user workouts
+            #the show page for each work out that isn't created by current user does not have edit or delete actions
+            #what condition can I write on the erb to only edit my workouts and not someone else's?
+            #if this particular workout is not connected to the current user do not show edit or delete? otherwise show me.
